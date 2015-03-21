23e9f7079671942a7d98e9dbf7e690337ba39d01
webduvet <andrej.bartko@gmail.com>
2015-03-21 00:09:32 +0000

    more thoughts

---
+++ obj.js
@@ -0,0 +1,69 @@
+/*
+ * object.watch polyfill
+ *
+ * 2012-04-03
+ *
+ * By Eli Grey, http://eligrey.com
+ * Public Domain.
+ * NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
+ */
+
+// object.watch
+
+
+var MyClass = function(){
+};
+
+
+
+if (!MyClass.prototype.watch) {
+	Object.defineProperty(MyClass.prototype, "watch", {
+		  enumerable: false
+		, configurable: true
+		, writable: false
+		, value: function (prop, handler) {
+			var
+			  oldval = this[prop]
+			, newval = oldval
+			, getter = function () {
+				return newval;
+			}
+			, setter = function (val) {
+				oldval = newval;
+				return newval = handler.call(this, prop, oldval, val);
+			}
+			;
+			
+			if (delete this[prop]) { // can't watch constants
+				Object.defineProperty(this, prop, {
+					  get: getter
+					, set: setter
+					, enumerable: true
+					, configurable: true
+				});
+			}
+		}
+	});
+}
+
+// object.unwatch
+if (!MyClass.prototype.unwatch) {
+	Object.defineProperty(MyClass.prototype, "unwatch", {
+		  enumerable: false
+		, configurable: true
+		, writable: false
+		, value: function (prop) {
+			var val = this[prop];
+			delete this[prop]; // remove accessors
+			this[prop] = val;
+		}
+	});
+}
+
+
+var obj = new MyClass();
+
+obj.test = 10;
+obj.watch('test', function(){console.log("test has changed")});
+
+obj.test = 20;
