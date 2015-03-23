6b83b73643da9e62c766975f8f2518b01991feb1
webduvet <andrej.bartko@gmail.com>
2015-03-23 23:44:55 +0000

    mapper version 0

--- sample.js
+++ sample.js
@@ -1,9 +1,14 @@
 var Firebase = require('firebase');
 var Zz = require('./lib/mapper.js');
 var ref = new Firebase('https://sagavera.firebaseio.com');
+var blueprint = {
+  prop1: null,
+  prop2: null,
+  prop3: "default"
+};
 
 
-var x = new Zz.Value(ref.child('test'), 'test/me', null);
+var x = new Zz.Model(ref.child('test'), 'test/me', blueprint);
 
 
 console.log(x);
