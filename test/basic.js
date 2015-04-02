3515f6e9c300f921585ab95204ff3bf85c23e5f6
Horse <andrej.bartko@gmail.com>
2015-04-02 14:25:12 +0100

    renamed Zz to Fm, simple test in place

---
+++ test/basic.js
@@ -0,0 +1,22 @@
+process.on('uncaugthException', function(err){
+	console.error(err.stack);
+});
+
+var Fm = require('../build/firebase-mapper.js');
+
+module.exports = {
+	'test basic': {
+		'setUp': function(done) {
+			done();
+		},
+		'all in place': function(test) {
+			test.expect(5);
+			test.equals(typeof Fm, 'object', 'expect object got: ', typeof Fm);
+			test.equals(typeof Fm.Model, 'function', 'expect typeof function and got', typeof Fm.Model);
+			test.equals(typeof Fm.ModelFactory, 'function', 'expect typeof function and got', typeof Fm.ModelFactory);
+			test.equals(typeof Fm.List, 'function', 'expect typeof function and got', typeof Fm.List);
+			test.equals(typeof Fm.ListFactory, 'function', 'expect typeof function and got', typeof Fm.ListFactory);
+			test.done();
+		}
+	}
+};
