0a3c244be8852635eb2d85a096b3b2fa63790b4c
webduvet <andrej.bartko@gmail.com>
2015-04-04 12:10:06 +0100

    priority provider and test of it

--- test/basic.js
+++ test/basic.js
@@ -3,6 +3,7 @@ process.on('uncaugthException', function(err){
 });
 
 var Fm = require('../build/firebase-mapper.js');
+var Firebase = require('firebase');
 
 module.exports = {
 	'test basic': {
@@ -19,10 +20,22 @@ module.exports = {
 			test.done();
 		},
 		'test exceptions': function(test) {
-			test.expect(3);
+			test.expect(6);
 			test.throws( function(){ new Fm.List();});
 			test.throws( function(){ new Fm.Model();});
 			test.throws( function(){ new Fm.Model({},{});});
+			test.doesNotThrow( function(){ new Fm.Model(new Firebase('https://sagavera.firebaseio.com'),{prop:"sample"});});
+			test.throws( function(){ new Fm.Model(new Firebase('https://sagavera.firebaseio.com'),null);});
+			test.throws( function(){ new Fm.Model({},{prop:"sample"});});
+			test.done();
+		},
+		'test priority provider': function(test) {
+			var ts1 = Date.now();
+			test.expect(1);
+			var provider = new Fm.PriorityProvider(function(){
+				return Date.now();
+			});
+			test.ok(ts1 < provider.priority < Date.now(), 'expect unix timestamp');
 			test.done();
 		}
 	}
