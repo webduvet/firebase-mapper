60cfe227cdff61ac5a8c9f6ccc5f536cc568e677
Horse <andrej.bartko@gmail.com>
2015-04-10 14:06:18 +0100

    debugging saving references... pain...

--- test/basic.js
+++ test/basic.js
@@ -42,7 +42,7 @@ module.exports = {
 			done();
 		},
 		'all in place': function(test) {
-			test.expect(7);
+			test.expect(10);
 			test.equals(typeof Fm, 'object', 'expect object got: ', typeof Fm);
 			test.equals(typeof Fm.Model, 'function', 'expect typeof function and got', typeof Fm.Model);
 			test.equals(typeof Fm.ModelFactory, 'function', 'expect typeof function and got', typeof Fm.ModelFactory);
@@ -50,6 +50,9 @@ module.exports = {
 			test.equals(typeof Fm.ListFactory, 'function', 'expect typeof function and got', typeof Fm.ListFactory);
 			test.equals(typeof Fm.Reference, 'function', 'expect typeof function and got', typeof Fm.Reference);
 			test.equals(typeof Fm.ReferenceFactory, 'function', 'expect typeof function and got', typeof Fm.ReferenceFactory);
+			test.ok(Fm.Model.prototype instanceof Fm.Basic, 'Fm.Model is expected to inherit from Fm.Basic');
+			test.ok(Fm.Reference.prototype instanceof Fm.Basic, 'Fm.Reference is expected to inherit from Fm.Basic');
+			test.ok(Fm.ReferenceFactory.prototype instanceof Fm.ModelFactory, 'Fm.Reference is expected to inherit from Fm.Basic');
 			test.done();
 		},
 		'test exceptions': function(test) {
@@ -80,8 +83,11 @@ module.exports = {
 			//});
 		},
 		'simple': function (test) {
-			test.expect(2);
+			test.expect(4);
 			var model = new Fm.Model(this.ref.child('simple'), testBP.simple);
+
+			test.ok(model instanceof Fm.Model, 'expect model to be instance of Fm.Model');
+			test.ok(model instanceof Fm.Basic, 'expect model to be instance of Fm.Basic');
 			// TODO if not write then watchLocal does not trigger written..
 			// model.write();
 			test.equals(model.prop1, "prop1", "Expect prop1 and got " + model.prop1);
@@ -125,12 +131,16 @@ module.exports = {
 			test.done();
 		},
 		'modelWithNestedReferenceList': function(test) {
-			test.expect(1);
+			test.expect(3);
 
 			var m = new Fm.Model(this.ref.child('nestedReferenceList'), testBP.nestedReferenceList);
-			console.log(m);
 
 			test.ok( m.prop2 instanceof Fm.List, "Expect instance of a Fm.List" );
+			test.ok( m.prop2.factory instanceof Fm.ReferenceFactory, "factory in list is expected to be instance of ReferenceFactory");
+			test.ok( m.prop2.factory instanceof Fm.ModelFactory, "factory in list is expected to be instance of ModelFactory");
+
+			m.prop2.add('testref',true);
+
 			test.done();
 		}
 	}
