eb7e2f7e73bfddfd9d9c0df7a0ed70d217b3e39f
Horse <andrej.bartko@gmail.com>
2015-04-22 13:25:30 +0100

    gmm

--- test/basic.js
+++ test/basic.js
@@ -4,7 +4,7 @@ process.on('uncaugthException', function(err){
 
 var Firebase = require('firebase');
 //var Fm = require('../build/firebase-mapper.js')(Firebase);
-var Fm = require('../build/firebase-mapper.js');
+var Fm = require('../build/firebase-mapper');
 
 var testBP = {
 	simple: {
